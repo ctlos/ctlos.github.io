e682d13431dd6cd3bb96792e40b5dce3f17af9d4
creio <mailcreio@gmail.com>
2021-02-04 19:09:21 +0300

    04.02.2021 Update

--- _includes/js/custom.js
+++ _includes/js/custom.js
@@ -10,4 +10,3 @@
 // })(document, window, 'Yeps');
 
 // help chat
-window.$crisp=[];window.CRISP_WEBSITE_ID="8e987c59-e515-4807-92c5-b1c7db4edd3d";(function(){d=document;s=d.createElement("script");s.src="https://client.crisp.chat/l.js";s.async=1;d.getElementsByTagName("head")[0].appendChild(s);})();
