d6af1156a9f40588aab28ef0281f4849e7c56b66
creio <mailcreio@gmail.com>
2025-03-06 15:54:05 +0300

    06.03.2025 Update

---
+++ assets/js/custom.js
@@ -0,0 +1,33 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+  $('.slider-reviews').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    lazyLoad: 'progressive',
+    autoplay: true,
+    autoplaySpeed: 6000,
+    speed: 1800,
+    dots: true,
+    responsive: [
+      {
+        breakpoint: 1399,
+        settings: {
+          slidesToShow: 3,
+        }
+      },
+      {
+        breakpoint: 1199,
+        settings: {
+          slidesToShow: 2,
+        }
+      },
+      {
+        breakpoint: 767,
+        settings: {
+          slidesToShow: 1,
+        }
+      },
+    ]
+  });
+
+});
